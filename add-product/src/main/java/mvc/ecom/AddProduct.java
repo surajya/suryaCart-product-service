fc96dc1900e9762b9aa9650f9d103db26ce19a90
surajvns1571999 <surajvns1571999@gmail.com>
2025-12-14 12:10:54 +0530

    suryacart project setup

---
+++ add-product/src/main/java/mvc/ecom/AddProduct.java
@@ -0,0 +1,14 @@
+package mvc.ecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddProduct {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddProduct.class, args);
+		System.out.println("Add-Product Application started.............");
+	}
+
+}
