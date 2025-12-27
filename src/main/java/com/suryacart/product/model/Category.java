5365ee5784aa9d361fd4db46fa81a1f8b8f7af0e
surajvns1571999 <surajvns1571999@gmail.com>
2025-12-27 07:58:02 +0530

    Add Created and Update Field in Category class

--- src/main/java/com/suryacart/product/model/Category.java
+++ src/main/java/com/suryacart/product/model/Category.java
@@ -1,5 +1,6 @@
 package com.suryacart.product.model;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
@@ -10,6 +11,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -35,4 +38,18 @@ public class Category {
 	@OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
 	@JsonIgnore
 	private List<Product> products;
+
+	private LocalDateTime createdAt;
+	private LocalDateTime updatedAt;
+
+	@PrePersist
+	void onCreate() {
+		this.createdAt = LocalDateTime.now();
+		this.updatedAt = LocalDateTime.now();
+	}
+
+	@PreUpdate
+	void onUpdate() {
+		this.updatedAt = LocalDateTime.now();
+	}
 }
